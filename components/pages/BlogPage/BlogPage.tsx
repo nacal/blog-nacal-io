bddc162d9326fd490bf4004cb7a87a6cb0763c4f
nacal <nacal.dev@gmail.com>
2022-12-31 22:01:50 +0900

    develop BlogPage

--- components/pages/BlogPage/BlogPage.tsx
+++ components/pages/BlogPage/BlogPage.tsx
@@ -1,6 +1,6 @@
 import { Entry } from 'contentful'
 import { FC } from 'react'
-import styles from './indexPage.module.scss'
+import styles from './BlogPage.module.scss'
 import BlogPost from 'components/organisms/BlogPost'
 import { IPostFields } from 'lib/contentful'
 
@@ -10,11 +10,11 @@ type Props = {
 
 const BlogPage: FC<Props> = ({ posts }) => {
   return (
-    <>
+    <section className={styles['posts']}>
       {posts.map((post: Entry<IPostFields>) => (
         <BlogPost id={post.sys.id} title={post.fields.title} createdAt={post.sys.createdAt} key={post.sys.id} />
       ))}
-    </>
+    </section>
   )
 }
 
