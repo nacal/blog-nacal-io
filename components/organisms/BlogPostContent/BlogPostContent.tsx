60e8e43756b79faa9e446244bb3f670b610b8b8e
nacal <nacal.dev@gmail.com>
2023-01-01 00:52:34 +0900

    develop BlogPostContent style

--- components/organisms/BlogPostContent/BlogPostContent.tsx
+++ components/organisms/BlogPostContent/BlogPostContent.tsx
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
 import { FC } from 'react'
+import markdownHtml from 'zenn-markdown-html'
 import styles from './BlogPostContent.module.scss'
+import 'zenn-content-css'
 
 type Props = {
   title: string
@@ -9,11 +11,20 @@ type Props = {
 }
 
 const BlogPostContent: FC<Props> = ({ title, createdAt, body }) => {
+  const html = markdownHtml(body)
+
   return (
-    <article className={styles['post']}>
-      <h2 className={styles['title']}>{title}</h2>
-      <time className={styles['time']}>{dayjs(createdAt).format('YYYY-MM-DD')}</time>
-      <div>{body}</div>
+    <article className={styles['article']}>
+      <div className={styles['content']}>
+        <h2 className={styles['title']}>{title}</h2>
+        <time className={styles['time']}>{dayjs(createdAt).format('YYYY-MM-DD')}</time>
+        <div
+          className={`${styles['body']} znc`}
+          dangerouslySetInnerHTML={{
+            __html: html,
+          }}
+        />
+      </div>
     </article>
   )
 }
