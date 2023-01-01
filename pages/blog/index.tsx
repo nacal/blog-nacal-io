da0a940c2cca7d47b6a26a9c79846326c2364dc5
nacal <nacal.dev@gmail.com>
2023-01-02 01:31:42 +0900

    format publishedAt at getStaticProps

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,6 @@
 import { Entry, EntryCollection } from 'contentful'
+import dayjs from 'dayjs'
 import type { NextPage, GetStaticProps } from 'next'
-import Head from 'next/head'
 import DefaultLayout from 'components/layout/Default'
 import BlogPageContainer from 'components/pages/BlogPage'
 import { buildClient, IPostFields } from 'lib/contentful'
@@ -14,6 +14,11 @@ export const getStaticProps: GetStaticProps = async () => {
     order: '-sys.createdAt',
   })
   publishRssXml(items, '/blog')
+  items.map(
+    (item: Entry<IPostFields>) =>
+      (item.fields.publishedAt = dayjs(item.fields.publishedAt).tz('Asia/Tokyo').format('YYYY-MM-DD'))
+  )
+
   return {
     props: { posts: items },
   }
