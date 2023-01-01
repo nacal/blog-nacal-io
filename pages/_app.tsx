339b9156062a6b239b6023d9d7e3d6035434332f
nacal <nacal.dev@gmail.com>
2023-01-02 01:27:36 +0900

    add dayjs settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.scss'
 import '../styles/reset.scss'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
+
+dayjs.extend(timezone)
+dayjs.extend(utc)
+dayjs.tz.setDefault('Asia/Tokyo')
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
