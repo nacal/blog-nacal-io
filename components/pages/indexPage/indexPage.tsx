8b03a90beb5f4c6517b722ac2eaed0088d08e04c
nacal <nacal.dev@gmail.com>
2023-01-15 00:00:31 +0900

    delete main

--- components/pages/indexPage/indexPage.tsx
+++ components/pages/indexPage/indexPage.tsx
@@ -4,24 +4,22 @@ import styles from './indexPage.module.scss'
 const IndexPage = () => {
   return (
     <>
-      <main>
-        <div className={styles['contents']}>
-          <div>
-            <div className={styles['links']}>
-              <a href="https://github.com/nacal" className={styles['icon']}>
-                <FaGithubSquare title="GitHub icon" />
-              </a>
-              <a href="https://twitter.com/_nacal" className={styles['icon']}>
-                <FaTwitterSquare title="Twitter icon" />
-              </a>
-            </div>
-            <h2 className={styles['copy']}>Under development.</h2>
-            <address className={styles['address']}>
-              <a href="mailto:nacal.dev@gmail.com">nacal.dev@gmail.com</a>
-            </address>
+      <div className={styles['contents']}>
+        <div>
+          <div className={styles['links']}>
+            <a href="https://github.com/nacal" className={styles['icon']}>
+              <FaGithubSquare title="GitHub icon" />
+            </a>
+            <a href="https://twitter.com/_nacal" className={styles['icon']}>
+              <FaTwitterSquare title="Twitter icon" />
+            </a>
           </div>
+          <h2 className={styles['copy']}>Under development.</h2>
+          <address className={styles['address']}>
+            <a href="mailto:nacal.dev@gmail.com">nacal.dev@gmail.com</a>
+          </address>
         </div>
-      </main>
+      </div>
     </>
   )
 }
