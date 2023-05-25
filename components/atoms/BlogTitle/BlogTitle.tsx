b6298564fe030a2a374746fd2adeec81f9ff3589
nacal <nacal.dev@gmail.com>
2023-05-25 16:33:02 +0900

    ts-ignore

--- components/atoms/BlogTitle/BlogTitle.tsx
+++ components/atoms/BlogTitle/BlogTitle.tsx
@@ -9,6 +9,7 @@ type Props = {
 
 const BlogTitle: FC<Props> = ({ children, type, viewTransitionName }) => {
   return (
+    // @ts-ignore
     <h2 className={`${styles.title} ${type && styles.bold}`} style={{ viewTransitionName: viewTransitionName }}>
       {children}
     </h2>
