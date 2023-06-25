e3ee3e5e8c5c7a5d8ef543a6eeb37512c7e7d978
nacal <nacal.dev@gmail.com>
2023-06-25 20:22:54 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,6 @@ import DefaultLayout from 'components/layout/Default'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>nacal.io</title>
-        <meta name="description" content="nacal.io" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <HeadContents title={'nacal.io'} description={"nacal's web site."} url={process.env.BASE_URL!} />
       <DefaultLayout>
         <IndexPage />
