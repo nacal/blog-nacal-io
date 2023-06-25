40a7e404d487f8c07999b3aa63987e9115e57db3
nacal <55532835+nacal@users.noreply.github.com>
2023-06-25 21:08:13 +0900

    Revert "TOPページOGP更新"

--- components/layout/Default/DefaultLayout.tsx
+++ components/layout/Default/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import Spline from '@splinetool/react-spline'
+import Head from 'next/head'
 import { FC, memo, ReactNode } from 'react'
 import styles from './DefaultLayout.module.scss'
 import Heading from 'components/atoms/Heading'
@@ -10,6 +11,10 @@ type Props = {
 const DefaultLayout: FC<Props> = memo(({ children }) => {
   return (
     <>
+      <Head>
+        {/* eslint-disable-next-line */}
+        <script src="https://embed.zenn.studio/js/listen-embed-event.js" />
+      </Head>
       <main className={styles['main']}>
         <Heading />
         <Spline scene="https://prod.spline.design/E7QFjnk3egxvGUGL/scene.splinecode" className={styles['spline']} />
